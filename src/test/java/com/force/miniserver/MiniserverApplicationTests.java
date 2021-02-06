a3be5632f7547727ddd8e4d3693e23a55ff3d361
balanice <tokuu@outlook.com>
2021-02-06 14:59:50 +0800

    Init project

---
+++ src/test/java/com/force/miniserver/MiniserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.force.miniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
